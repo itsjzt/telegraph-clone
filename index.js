8e7ebdc42df866825397d07a04168d9678f1d0fb
itsjzt <saurabh.19@outlook.com>
2018-03-31 11:06:33 +0530

    styles, pug, body parser working.

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ require("dotenv").config();
 require("./model/connect");
 
 const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 const Blog = require("./model/blog");
 
-// const first = new Blog({ title: "dope", username: "hi", post: "hi" });
+// express configs
+app.set("views", "./views");
+app.set("view engine", "pug");
+app.use("/static", express.static(path.join(__dirname, "static")));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// const first = new Blog({ title: "cool", username: "hi", post: "hi" });
 // first.save((err, res) => {
 //   if (err) console.error("error", err);
 //   else console.log("done");
 // });
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) =>
+  res.render("index", { title: "Hey", message: "Hello there!" })
+);
+
+app.post("/post", (req, res) => {
+  res.send(req.body);
+});
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
